19be086094fa19c88790662414472d33d6bf2bcb
sedfsdfsdasdasdf <73028283+sedfsdfsdasdasdf@users.noreply.github.com>
2022-07-14 22:50:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ chat_id = '-1001597696937'
 
 """Add cookies for the requests.get"""
 cookies = {'PHPSESSID': 'bimr9fcf952l5dmvh55pf5qlpc',
+           'hc_accessibility': 'EDsP2bDIggc+3a5VW5gfhTxz4+7vgp7geQ5yTkeTYelbaJJJNwvD84VmKddjmUiUvp3E8vqF3mOkRAQlXVETI7u1TjADlL85dXo7aMjJXyv5tzQWCn4Ed06BxB0Hj2wlsXIAUVgqmO+1iN+xO8SpcVSN8ooK8KI91OaOiDD/nv0W0SjT8Hbn2KXG0cbrsATI94te50ofjKXjRtscHcEZWkL3IB0DEm5MopK3j9qX4w70mCfwEThTXWNSsscIm3/eTT8UxhWXXY8ePXmAMl5Ox9qykNsvz5WkvGsf4A0LhN5I4OUpaXaj/BclSaNEhhtf2xnIyRR/wbW05SBMqzSB9fgBIrMdl/0/YkeoBLkHn9hnQOWtww4GZonQKiJpBn0Q8bfnu0iwMjkZFUQ1aFje7WxrJQIa4IgEOVOqK+xm2mrp0vVfznjLElHqTDx/kCGecDqWdzEvfUEWOsZ4K4Fb7Gouls60hdkqpRbbSPmXUq3xzvvHlOHg2867/Ruksm6XLGThSNDbkYlz//j0Y332zzaKosMEPxQMgXAo3h9CHjlBKbWccPCiVh0IKvy6bpxNJRUc9O+6mXoSwlpx+9PsNl+2HQl3p4Vq1Mm3+vvjYv6qQWs2Q1CpppHgaJD/OVbRw2zkUio+kXjNNqx2fPTSK99zAXlvgXJOPlV5cUCRQSq4+5YG2qkLzhB7ymLHAC7LkDL6Gl6no+OY/WZn576/FUXg0NJqovrj17LaoCbNS2/a8p6/eMEzPScywzZGJL8UgVmCD+TaW5ofvHpH1c58ej4c1gkdIvpWxKTjs0k2sjkCDKVsaVp6d9GWLj/rHxv9x9rRvsJhUqLv5XuPFTS77qRXBYeGuUUzuoD2X0s9KebXUpzMkNryBK260OVvgUdZy0pXQFTEV2bVYe5JNaiWLDdp0pFIiU3t9izoceB0CGGAS/Gyp3P5Pib2uRYYL0scvCHMwzq9YKvb0dfF213pGkW00mvZr2TZSt413c6l744d5O+zWVXv++b0th5x6mNI94fc5y5GLGm7Shg7',
            'cf_clearance': 'IO4oAvHbHPk48k7TtAVK4jd1HTPx_Q2clDPQDjfZ0tw-1657711150-0-250',
            'csrfp_token': '33ed1ce66c',
            '__cf_bm': 'D2UO0.qDEdMXhiRyZN1KJ.mTHtCWeYPWWDx6Rbm19sA-1657712023-0-AbphBzWtNOU1Rw7bC9G0YP43Ebt51jZLl9nseW/ZO0OAYqBcygwGexsYXvLWo/wq3BWvKpgu8uIMpRIGbq6RmvbCTO26NTJpLZK8HS9qXbj5ugotljn2ytAj4LLx6cBZ4w==',
@@ -34,6 +35,13 @@ while True:
     try:
         r = requests.get(url, stream=True, headers=headers, cookies=cookies)
         soup = BeautifulSoup(r.text, 'html.parser')
+        if soup.find('form', class_='challenge-form interactive-form'):
+            print("Verification Needed")
+            while soup.find('form', class_='challenge-form interactive-form'):
+                r = requests.get(url, stream=True, headers=headers, cookies=cookies)
+                soup = BeautifulSoup(r.text, 'html.parser')
+                print("Refreshing...")
+                sleep(5)
         posts = soup.find_all('table', {'class': 'tborder latestthreads_table'})
         for post in posts:
             title = post.find('span', {'class': 'post_link'}).find('a').text
@@ -61,7 +69,7 @@ while True:
 
             """if src of profile_picture finishes with "default_avatar.png" or with "transparent.png", remove "https://static.cracked.io/" from start"""
             if profile_picture.endswith("default_avatar.png") or profile_picture.endswith("transparent.png"):
-                profile_picture = profile_picture.replace("https://static.cracked.io/", "")
+                profile_picture = "https://static.cracked.io/" + profile_picture.replace("https://static.cracked.io/", "")
             
             elif not profile_picture.endswith("default_avatar.png") or not profile_picture.endswith("transparent.png"):
                 profile_picture = profile_picture.replace("./", "").replace("/avatars/a", "/avatars//a").split("?")[0].replace("\n", "")
@@ -85,14 +93,14 @@ while True:
                 
             else:
                 break
-        sleep(2)
+        sleep(3)
     
     except requests.exceptions.ChunkedEncodingError:
         print("Error ChunkedEncodingError")
-        sleep(3)
+        sleep(5)
         continue
     
     except requests.exceptions.ConnectionError:
         print("Error ConnectionError")
-        sleep(3)
+        sleep(5)
         continue
